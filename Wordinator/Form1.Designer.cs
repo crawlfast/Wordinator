0298f1528310fdfde41d3b3d39906c4847ce397a
crawlfast <crawlfast@ILQ>
2017-01-10 20:58:18 +0200

    Add project files.

---
+++ Wordinator/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace test
+{
+    partial class Wordinator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterPercentLabel = new System.Windows.Forms.Label();
+            this.percentTextBox = new System.Windows.Forms.TextBox();
+            this.percentLabel = new System.Windows.Forms.Label();
+            this.enterTextLabel = new System.Windows.Forms.Label();
+            this.primaryTextBox = new System.Windows.Forms.TextBox();
+            this.letterChangeButton = new System.Windows.Forms.Button();
+            this.changedTextBox = new System.Windows.Forms.TextBox();
+            this.changedTextLabel = new System.Windows.Forms.Label();
+            this.wordChangeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // enterPercentLabel
+            // 
+            this.enterPercentLabel.AutoSize = true;
+            this.enterPercentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterPercentLabel.Location = new System.Drawing.Point(22, 29);
+            this.enterPercentLabel.Name = "enterPercentLabel";
+            this.enterPercentLabel.Size = new System.Drawing.Size(268, 16);
+            this.enterPercentLabel.TabIndex = 0;
+            this.enterPercentLabel.Text = "Įveskite, kiek procentų teksto norite pakeisti:";
+            // 
+            // percentTextBox
+            // 
+            this.percentTextBox.Location = new System.Drawing.Point(359, 28);
+            this.percentTextBox.Name = "percentTextBox";
+            this.percentTextBox.Size = new System.Drawing.Size(51, 20);
+            this.percentTextBox.TabIndex = 1;
+            // 
+            // percentLabel
+            // 
+            this.percentLabel.AutoSize = true;
+            this.percentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.percentLabel.Location = new System.Drawing.Point(416, 29);
+            this.percentLabel.Name = "percentLabel";
+            this.percentLabel.Size = new System.Drawing.Size(20, 16);
+            this.percentLabel.TabIndex = 2;
+            this.percentLabel.Text = "%";
+            // 
+            // enterTextLabel
+            // 
+            this.enterTextLabel.AutoSize = true;
+            this.enterTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterTextLabel.Location = new System.Drawing.Point(22, 110);
+            this.enterTextLabel.Name = "enterTextLabel";
+            this.enterTextLabel.Size = new System.Drawing.Size(209, 16);
+            this.enterTextLabel.TabIndex = 3;
+            this.enterTextLabel.Text = "Įveskite tekstą, kurį norite pakeisti:";
+            // 
+            // primaryTextBox
+            // 
+            this.primaryTextBox.Location = new System.Drawing.Point(25, 146);
+            this.primaryTextBox.Multiline = true;
+            this.primaryTextBox.Name = "primaryTextBox";
+            this.primaryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.primaryTextBox.Size = new System.Drawing.Size(473, 209);
+            this.primaryTextBox.TabIndex = 4;
+            // 
+            // letterChangeButton
+            // 
+            this.letterChangeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.letterChangeButton.Location = new System.Drawing.Point(25, 380);
+            this.letterChangeButton.Name = "letterChangeButton";
+            this.letterChangeButton.Size = new System.Drawing.Size(227, 36);
+            this.letterChangeButton.TabIndex = 5;
+            this.letterChangeButton.Text = "Keisti raides";
+            this.letterChangeButton.UseVisualStyleBackColor = true;
+            this.letterChangeButton.Click += new System.EventHandler(this.letterChangeButton_Click);
+            // 
+            // changedTextBox
+            // 
+            this.changedTextBox.Location = new System.Drawing.Point(25, 485);
+            this.changedTextBox.Multiline = true;
+            this.changedTextBox.Name = "changedTextBox";
+            this.changedTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.changedTextBox.Size = new System.Drawing.Size(473, 209);
+            this.changedTextBox.TabIndex = 7;
+            // 
+            // changedTextLabel
+            // 
+            this.changedTextLabel.AutoSize = true;
+            this.changedTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changedTextLabel.Location = new System.Drawing.Point(22, 454);
+            this.changedTextLabel.Name = "changedTextLabel";
+            this.changedTextLabel.Size = new System.Drawing.Size(147, 16);
+            this.changedTextLabel.TabIndex = 8;
+            this.changedTextLabel.Text = "Jūsų pakeistas tekstas:";
+            // 
+            // wordChangeButton
+            // 
+            this.wordChangeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordChangeButton.Location = new System.Drawing.Point(288, 379);
+            this.wordChangeButton.Name = "wordChangeButton";
+            this.wordChangeButton.Size = new System.Drawing.Size(210, 37);
+            this.wordChangeButton.TabIndex = 9;
+            this.wordChangeButton.Text = "Keisti žodžius";
+            this.wordChangeButton.UseVisualStyleBackColor = true;
+            this.wordChangeButton.Click += new System.EventHandler(this.wordChangeButton_Click);
+            // 
+            // Wordinator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(523, 730);
+            this.Controls.Add(this.wordChangeButton);
+            this.Controls.Add(this.changedTextLabel);
+            this.Controls.Add(this.changedTextBox);
+            this.Controls.Add(this.letterChangeButton);
+            this.Controls.Add(this.primaryTextBox);
+            this.Controls.Add(this.enterTextLabel);
+            this.Controls.Add(this.percentLabel);
+            this.Controls.Add(this.percentTextBox);
+            this.Controls.Add(this.enterPercentLabel);
+            this.Name = "Wordinator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wordinator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterPercentLabel;
+        private System.Windows.Forms.TextBox percentTextBox;
+        private System.Windows.Forms.Label percentLabel;
+        private System.Windows.Forms.Label enterTextLabel;
+        private System.Windows.Forms.TextBox primaryTextBox;
+        private System.Windows.Forms.Button letterChangeButton;
+        private System.Windows.Forms.TextBox changedTextBox;
+        private System.Windows.Forms.Label changedTextLabel;
+        private System.Windows.Forms.Button wordChangeButton;
+    }
+}
+
---
+++ Wordinator/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Wordinator : Form
+    {
+        private int textlength;
+        private char[] primaryText1 = new char[] { };
+
+        private int[] indexArray = new int[] { };
+        private int percent = 0;
+        private string[] primaryText2 = new string[] { };
+
+        Random rng = new Random();
+        private string[] changedText2 = new string[] { };
+
+        public Wordinator()
+        {
+            InitializeComponent();
+        }
+
+        private void letterChangeButton_Click(object sender, EventArgs e)
+        {
+
+            GetTextInChars(out textlength, out primaryText1);
+            int percent = GetPercentage(textlength);
+            indexArray = GetRandomNumbers(percent, textlength, indexArray);
+            char[] tempArray = FindElements1();
+            Shuffle(indexArray);
+            char [] changedText1 = ChangeText1(tempArray);
+
+            //changedTextBox.Text = percent.ToString();
+            changedTextBox.Text = /*string.Join(", ", indexArray) + " " + string.Join(", ", tempArray) + " " +
+                string.Join(", ", indexArray) + " " +*/
+                string.Join("", changedText1); 
+   
+        }
+
+        private void wordChangeButton_Click(object sender, EventArgs e)
+        {
+            GetTextInWords(out textlength, out primaryText2);
+            int percent = GetPercentage(textlength);
+            indexArray = GetRandomNumbers(percent, textlength, indexArray);
+
+            string[] tempArray = FindElements2();
+            int k = 0;
+            int oldIndexValue = 0;
+            if (indexArray.Length == 1)
+            {
+                k = rng.Next(0, textlength);
+                oldIndexValue = indexArray[0]; 
+                indexArray[0] = k;
+            }
+            else
+            {
+                Shuffle(indexArray);
+            }
+            List<string> list = new List<string>();
+            string[] changedText2;
+            if (indexArray.Length!=1)
+            {
+                changedText2 = ChangeText2(tempArray);
+            }
+            else
+            {
+                
+                list = primaryText2.OfType<string>().ToList();
+                list.RemoveAt(oldIndexValue);
+                list.Insert(k, primaryText2[oldIndexValue]);
+                changedText2 = list.ToArray();
+            }
+            
+
+            changedTextBox.Text = /*/*percent.ToString() string.Join(", ", indexArray) + " " + string.Join(", ", tempArray) + " " +
+                string.Join(", ", indexArray) + " " +*/
+                string.Join(" ", changedText2);
+            
+        }
+
+
+        
+        private void GetTextInChars(out int textlength, out char[] primaryText1)
+        {
+            string primaryChars = primaryTextBox.Text;
+            primaryText1 = primaryChars.ToCharArray();
+            textlength = primaryText1.Length;
+        }
+
+        private void GetTextInWords(out int textlength, out string[] primaryText2)
+        {
+            //char[] e = { ' ', ',', '.', ':', ';', '!', '?', '<', '>', '/', '\\', '\t', '\n', '\'', '\"', '-' };
+            primaryText2 = primaryTextBox.Text.Split(' ');
+            textlength = primaryText2.Length;
+        }
+
+        private int GetPercentage(int textlength)
+        {
+            int x;
+            Int32.TryParse(percentTextBox.Text, out x);
+            percent = x * textlength / 100;
+            return percent;
+        }
+
+        private int[] GetRandomNumbers(int percent, int textlength, int[] indexArray)
+        {
+
+            List<int> elements = new List<int>();
+            Random nr = new Random();
+
+            for (int i = 0; i < percent;)
+            {
+
+                int randomNumber = nr.Next(0, textlength);
+
+                while (!(elements.Contains(randomNumber)))
+                {
+                    elements.Add(randomNumber);
+                    i++;
+                }
+            }
+            indexArray = elements.ToArray();
+            return indexArray;
+        }
+
+        private char[] FindElements1()
+        {
+            List<char> tempList1 = new List<char>();
+
+            for (int j = 0; j < indexArray.Length; j++)
+            {
+                int indexValue = indexArray[j];
+                if (indexValue != Array.IndexOf(primaryText1, j))
+                {
+                    char tempElement = primaryText1[indexValue];
+                    tempList1.Add(tempElement);
+                }
+            }
+            char[] tempArray = tempList1.ToArray();
+            return tempArray;
+        }
+
+        private string[] FindElements2()
+        {
+            List<string> tempList2 = new List<string>();
+
+            for (int j = 0; j < indexArray.Length; j++)
+            {
+                int indexValue = indexArray[j];
+                if (indexValue != Array.IndexOf(primaryText2, j))
+                {
+                    string tempElement = primaryText2[indexValue];
+                    tempList2.Add(tempElement);
+                }
+            }
+            string[] tempArray = tempList2.ToArray();
+            return tempArray;
+        }
+        private void Shuffle<T>(T[] indexArray)
+        {
+
+            int n = indexArray.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = indexArray[n];
+                indexArray[n] = indexArray[k];
+                indexArray[k] = temp;        
+            }
+        }
+
+        private char[] ChangeText1(char[] tempArray)
+        {
+            for (int g = 0; g < indexArray.Length; g++)
+            {
+                int shuffledIndexValue = indexArray[g];
+                char newTextValue = tempArray[g];
+
+                primaryText1[shuffledIndexValue] = newTextValue;
+
+            }
+            return primaryText1;
+        }
+
+        private string[] ChangeText2(string[] tempArray)
+        {
+            for (int g = 0; g < indexArray.Length; g++)
+            {
+                int shuffledIndexValue = indexArray[g];
+                string newTextValue = tempArray[g];
+
+                primaryText2[shuffledIndexValue] = newTextValue;
+
+            }
+            return primaryText2;
+        }
+
+    }
+
+}
